0dd41cd774d8034697e5984e56405c0bc2c7e9af
Arthur Dhuy <a.dhuy@orange.fr>
2018-12-17 23:07:29 +0100

    Fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var path = require('path');
+const webpack = require("webpack");
+const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   }
 };
